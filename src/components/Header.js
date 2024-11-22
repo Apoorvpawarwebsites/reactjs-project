11ab1a89fd16dad751666a6881274a161261eaa0
Apoorvpawarwebsites <114346887+Apoorvpawarwebsites@users.noreply.github.com>
2024-11-22 13:27:05 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+
+    return (
+
+        <header className="header">
+            <div className="header-content">
+
+                <div className="header-text">
+                    <h1>
+                        Hi There, I am <span className="name">Jony</span>
+                    </h1>
+                    <h2 className="role">Creative Director</h2>
+                    <button className="View">View Works</button>
+                </div>
+
+                <div className="header-image">
+                    <img src="./images/Jony.png" alt="Jony" />
+                </div>
+
+            </div>
+        </header >
+    );
+
+};
+
+export default Header;
---
+++ src/components/Navbar.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { FaBars, FaTimes, FaArrowDown } from "react-icons/fa";
+import "./Navbar.css";
+
+
+const Navbar = () => {
+
+
+    const [isOpen, setIsOpen] = useState(false);
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
+    const toggleMenu = () => {
+
+        setIsOpen(!isOpen);
+    }
+
+    const toggleDropdown = () => {
+
+        setDropdownOpen(!dropdownOpen);
+    }
+
+
+
+    return (
+
+        <nav className="navbar">
+
+            <div className="navbar-container">
+
+                <div className="logo">JONY.</div>
+
+                <div className="menu-icon" onClick={toggleMenu}>
+                    {isOpen ? <FaTimes /> : <FaBars />}
+                </div>
+
+                <ul className={`nav-links ${isOpen ? "active" : ""}`}>
+
+                    <li>Home</li>
+                    <li>Works</li>
+                    <li>Services</li>
+
+                    <li className="dropdown">
+                        <span onClick={toggleDropdown}>
+                            Pages <span className="dropdown-icon"><FaArrowDown /></span>
+                            {dropdownOpen && (
+
+                                <ul className="dropdown-menu">
+
+                                    <li>Work Details</li>
+                                    <li>Elements</li>
+                                </ul>
+                            )}
+                        </span>
+                    </li>
+
+                    <li>About</li>
+                    <li>BLog</li>
+                    <li>Contact Us</li>
+
+
+                </ul>
+
+                <button className="talk">Let's Talk</button>
+
+            </div>
+        </nav>
+    )
+
+
+};
+
+export default Navbar;
+
